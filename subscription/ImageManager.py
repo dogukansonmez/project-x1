33b3583e070ded01a3ba6c297e4088d0e431d609
dogukansonmez <dogukansonmez@gmail.com>
2013-11-16 15:09:55 +0100

    file upload limit

--- subscription/ImageManager.py
+++ subscription/ImageManager.py
@@ -26,33 +26,39 @@ def getUserFolder(currentUserName):
 def save_image_files(request):
     images = []
     i = 0
+    MAX_SIZE =3000000
     if request.FILES is not None:
         for fileName,file in request.FILES.iteritems():
-            name = generateFileName(file.name.split(".")[-1],i)
-            i= i+1
-            #TODO error check
-            userFolder = getUserFolder(get_user(request).username)
+           if file.size<MAX_SIZE:
+               #TODO give message about upload file
+                name = generateFileName(file.name.split(".")[-1],i)
 
-            imgFolder = createFolderForCurrentUser(userFolder)
+                i= i+1
+                #TODO error check
+                userFolder = getUserFolder(get_user(request).username)
 
-            images.append(userFolder + "/" + name)
-            # Open the image file.
-            img = Image.open(file)
-            # Resize it.
-            w = img.size[0]
-            h = img.size[1]
-            w_box = 750
-            h_box = 650
-            f1 = 1.0*w_box/w # 1.0 forces float division in Python2
-            f2 = 1.0*h_box/h
-            factor = min([f1, f2])
-            #print(f1, f2, factor) # test
-            # use best down-sizing filter
-            width = int(w*factor)
-            height = int(h*factor)
-            img = img.resize((width, height), Image.ANTIALIAS)
+                imgFolder = createFolderForCurrentUser(userFolder)
 
-            # Save it back to disk.
-            img.save(os.path.join(imgFolder, name))
+                images.append(userFolder + "/" + name)
 
+                try:
+                    # Open the image file.
+                    img = Image.open(file)
+                    # Resize it.
+                    w = img.size[0]
+                    h = img.size[1]
+                    w_box = 750
+                    h_box = 650
+                    f1 = 1.0*w_box/w # 1.0 forces float division in Python2
+                    f2 = 1.0*h_box/h
+                    factor = min([f1, f2])
+                    #print(f1, f2, factor) # test
+                    # use best down-sizing filter
+                    width = int(w*factor)
+                    height = int(h*factor)
+                    img = img.resize((width, height), Image.ANTIALIAS)
+                    # Save it back to disk.
+                    img.save(os.path.join(imgFolder, name))
+                except IOError:
+                    images.pop()
     return images
